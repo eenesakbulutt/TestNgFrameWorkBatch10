1344a964baaf75cefe28d43eaf3def3735556107
Enes Akbulut <eenesakbulutt@gmail.com>
2021-10-19 11:24:58 -0400

    TestNgFramework

---
+++ src/test/java/utils/Constants.java
@@ -0,0 +1,17 @@
+package utils;
+
+public class Constants {
+
+public static final String CONFIGURATION_FILEPATH=System.getProperty("user.dir")+ "/Users/kristinaakbulut/Desktop/TestNgFrameWorkBatch10/src/test/resources/config/config.properties";
+
+public static final int IMPLICIT_WAIT=10;
+
+
+
+
+
+
+
+
+
+}
